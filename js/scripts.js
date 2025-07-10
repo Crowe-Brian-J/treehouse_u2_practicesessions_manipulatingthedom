37accdcd2a70e56a40e2c73a35d9f825e4a2f05f
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-09 23:55:54 -0400

    exercises 1-4 complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
+//set heading so I'm not doing it 20x
+const heading = document.querySelector('h1')
+//random math for colors
+const randomNumber = () => Math.floor(Math.random() * 256)
+//random color picker
+const randomColorPicker = () => {
+  return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`
+}
+
 // 1: Set the text of the <h1> element
+heading.textContent = 'I created this.'
 
 // 2: Set the color of the <h1> to a different color
+heading.style.color = randomColorPicker()
 
 // 3: Set the content of the '.desc' paragraph
 // The content should include at least one HTML tag
+const descParagraph = document.getElementsByClassName('desc')[0]
+descParagraph.innerHTML = `This is <strong>strong</strong>. This is <i>italic</i>.`
 
 // 4: Set the class of the <ul> to 'list'
+const ul = document.getElementsByTagName('ul')[0]
+ul.className = 'list'
 
 // 5: Create a new list item and add it to the <ul>
 
